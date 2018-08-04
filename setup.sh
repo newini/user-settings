68eeb7cadf976d7563d160095306b8dbac5b00c8
Eunchong Kim <eunchong.kim@cern.ch>
2018-08-03 23:37:31 -0700

    Added setup.sh and vim cpp syn and latexmkrc

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Type "source setup.sh" on "user-settings" dir!
+#
+dirctory_name=user-settings
+
+# bashrc
+echo "" >> ../.bashrc
+echo "##" >> ../.bashrc
+echo "# bashrc user setting" >> ../.bashrc
+echo "##" >> ../.bashrc
+echo "    source \$HOME/$dirctory_name/bashrc" >> ../.bashrc
+echo "" >> ../.bashrc
+
+
+# Vim
+# vim settings
+echo "source \$HOME/$dirctory_name/vimrc" > ../.vimrc
+# vim cpp syntax
+mkdir -p ../.vim/after/syntax
+echo "source \$HOME/$dirctory_name/vim/after/syntax/cpp.vim" > ../.vim/after/syntax/cpp.vim
+
+
+# Latexmk
+cp latexmkrc ../.latexmkrc

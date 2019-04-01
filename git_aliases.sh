02f3e2d952bb24a6b3613effabbab1341d98efa4
Eunchong Kim <eunchong.kim@cern.ch>
2019-04-01 15:08:43 +0900

    Added git fetch and git prune at the same time

--- git_aliases.sh
+++ git_aliases.sh
@@ -5,6 +5,10 @@ alias gt='git tag '
 alias gc='git commit'
 alias gd='git diff'
 alias go='git checkout '
+gf() {
+    echo "git fetch $1 && git remote prune $1"
+    git fetch $1 && git remote prune $1
+}
 alias gl="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)'"
 alias gla="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 #alias gk='gitk --all&'
